59619f9fc68eed8672261240117a7ca0d5527ee8
YetiYetYet <alexisbehier22@gmail.com>
2020-12-08 02:05:42 +0100

    add up down animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,11 +11,18 @@ public class PlayerController : MonoBehaviour
     public int speed;
     public LayerMask movableLayer;
     public Vector3 playerOffset;
+    public float deltaY;
+    public float deltaZ;
+    public float animatorTolerance = 0.1f;
+
+    public Animator animator;
     
     [SerializeField]
     private Vector3 _target;
     [SerializeField]
     private Vector3 _mousePosition;
+
+    private Vector3 _initialRotation;
     
     private Ray _ray;
 
@@ -27,22 +34,18 @@ public class PlayerController : MonoBehaviour
     {
         _camera = Camera.main;
         _target = transform.position;
+        _initialRotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (_target.y > transform.position.y)
+        transform.eulerAngles = _initialRotation;
+        deltaY = _target.y - transform.position.y;
+        if (Math.Abs(deltaY) > animatorTolerance)
         {
-            Debug.Log("I go up");
-        }
-        else if (Math.Abs(_target.y - transform.position.y) < 0.1f)
-        {
-            Debug.Log("no y movement");
-        }
-        else
-        {
-            Debug.Log("move down");
+            Debug.Log(deltaY > 0);
+            animator.SetBool("MovingFoward", deltaY > 0);
         }
 
         _ray = _camera.ScreenPointToRay(Input.mousePosition);
